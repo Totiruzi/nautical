94b2320d9d2d61af1fd7fd5fc99f9819fffb594d
Totiruzi <totiruzi@yahoo.com>
2022-05-03 03:02:52 +0300

    Add angular material, header, login,signup and form skeleton

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,22 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { HeaderModule } from './header/header.module';
+import { SignupModule } from './auth/signup/signup.module';
+import { LoginModule } from './auth/login/login.module';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    HeaderModule,
+    LoginModule,
+    SignupModule
   ],
   providers: [],
   bootstrap: [AppComponent]
